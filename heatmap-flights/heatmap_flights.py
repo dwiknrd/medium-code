55659f1830cd43d20441f2e4b1f3eb059197578a
dwiknrd <dwiknrd@gmail.com>
2020-09-25 15:20:01 +0700

    heatmap code

--- heatmap-flights/heatmap_flights.py
+++ heatmap-flights/heatmap_flights.py
@@ -1,3 +1,7 @@
 import seaborn as sns 
 
 flights = sns.load_dataset("flights").pivot("month", "year", "passengers")
+sns.heatmap(flights)
+sns.heatmap(flights, fmt="d", annot=True, cmap='Blues')
+sns.heatmap(flights, fmt="d", cmap='YlGnBu')
+sns.heatmap(flights, center=flights.loc["Jan", 1955])
