0ba82ab24e3bf742696d576b2c8256388f07ed03
dwiknrd <dwiknrd@gmail.com>
2020-09-28 11:38:12 +0700

    handling missing values

--- netflix-eda/netflix_eda.py
+++ netflix-eda/netflix_eda.py
@@ -3,5 +3,35 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import seaborn as sns 
 
-#open dataset
+sns.set_style("darkgrid")
+matplotlib.rcParams["font.size"] = 14
+matplotlib.rcParams["figure.facecolor"] = '#00000000'
+
+#loading the dataset
 netflix_df = pd.read_csv("netflix_titles.csv")
+
+#Data Cleaning
+
+##Data with missing value
+print(netflix_df.T.apply(lambda x: x.isnull().sum(), axis = 1))
+##Handling missing value
+netflix_df.director.fillna("No Director", inplace=True)
+netflix_df.cast.fillna("No Cast", inplace=True)
+netflix_df.country.fillna("Country Unavailable", inplace=True)
+netflix_df.dropna(subset=["date_added", "rating"], inplace=True)
+
+#Netflix Content By Type
+
+#Amount of Content as a Function of Time
+
+#Countries by the Amount of the Produces Content
+
+#Top Directors on Netflix
+
+#Top Genres on Netflix
+
+#Amount of Content By Rating
+
+#Top Actor on Netflix
+
+#Movie Duration
