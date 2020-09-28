18090c15531dcdf80d58437f43d88a08ba6a12a9
dwiknrd <dwiknrd@gmail.com>
2020-09-28 11:48:37 +0700

    ploting amount of content as a function of time

--- netflix-eda/netflix_eda.py
+++ netflix-eda/netflix_eda.py
@@ -1,5 +1,6 @@
 import numpy as np
-import pandas as pd 
+import pandas as pd
+import matplotlib
 import matplotlib.pyplot as plt 
 import seaborn as sns 
 
@@ -20,18 +21,47 @@ netflix_df.cast.fillna("No Cast", inplace=True)
 netflix_df.country.fillna("Country Unavailable", inplace=True)
 netflix_df.dropna(subset=["date_added", "rating"], inplace=True)
 
-#Netflix Content By Type
+#1. Netflix Content By Type
+plt.figure(figsize=(12,6))
+plt.title("Percentation of Netflix Titles that are either Movies or TV Shows")
+g = plt.pie(netflix_df.type.value_counts(),explode=(0.025,0.025), labels=netflix_df.type.value_counts().index, colors=['red','black'],autopct='%1.1f%%', startangle=180)
+plt.show()
 
-#Amount of Content as a Function of Time
+#2. Amount of Content as a Function of Time
+netflix_movies_df = netflix_df[netflix_df.type.str.contains("Movie")]
+netflix_shows_df = netflix_df[netflix_df.type.str.contains("TV Show")]
+netflix_df.year_added = pd.DatetimeIndex(netflix_df.date_added).year
+netflix_movies_df.year_added = pd.DatetimeIndex(netflix_movies_df.date_added).year
+netflix_shows_df.year_added = pd.DatetimeIndex(netflix_shows_df.date_added).year
 
-#Countries by the Amount of the Produces Content
+netflix_year_df = netflix_df.year_added.value_counts().to_frame().reset_index().rename(columns={"index": "year", "year_added":"count"})
+netflix_year_df = netflix_year_df[netflix_year_df.year != 2020]
 
-#Top Directors on Netflix
+movies_year_df = netflix_movies_df.year_added.value_counts().to_frame().reset_index().rename(columns={"index": "year", "year_added":"count"})
+movies_year_df = movies_year_df[movies_year_df != 2020]
 
-#Top Genres on Netflix
+shows_year_df = netflix_shows_df.year_added.value_counts().to_frame().reset_index().rename(columns={"index": "year", "year_added":"count"})
+shows_year_df = shows_year_df[shows_year_df != 2020]
 
-#Amount of Content By Rating
+fig, ax = plt.subplots(figsize=(13, 7))
+sns.lineplot(data=netflix_year_df, x='year', y='date_added')
+sns.lineplot(data=movies_year_df, x='year', y='date_added')
+sns.lineplot(data=shows_year_df, x='year', y='date_added')
+ax.set_xticks(np.arange(2008, 2020, 1))
+plt.title("Total content added across all years (up to 2019)")
+plt.legend(['Total','Movie','TV Show'])
+plt.ylabel("Releases")
+plt.xlabel("Year")
+plt.show()
 
-#Top Actor on Netflix
+#3. Countries by the Amount of the Produces Content
 
-#Movie Duration
+#4. Top Directors on Netflix
+
+#5. Top Genres on Netflix
+
+#6. Amount of Content By Rating
+
+#7. Top Actor on Netflix
+
+#8. Movie Duration
