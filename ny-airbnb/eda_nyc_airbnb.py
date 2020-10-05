aec9a65b33939cb5ea5bf184d11b9b4a71abf491
dwiknrd <dwiknrd@gmail.com>
2020-10-05 20:10:22 +0700

    linier regression model

--- ny-airbnb/eda_nyc_airbnb.py
+++ ny-airbnb/eda_nyc_airbnb.py
@@ -81,9 +81,68 @@ print(nyc_df.isnull().any())
 import statsmodels.api as sm
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error,r2_score
+from sklearn import metrics
+from sklearn.metrics import mean_squared_error,r2_score, mean_absolute_error
 from sklearn import preprocessing
-from sklearn.feature_selection import RFE
+
 
 import warnings 
 warnings.filterwarnings('ignore')
+
+#encode label with value between 0 and n_classes-1
+encode = preprocessing.LabelEncoder()
+#fit label encoder
+encode.fit(nyc_df.neighbourhood_group)
+nyc_df.neighbourhood_group=encode.transform(nyc_df.neighbourhood_group)
+
+# Transform labels to normalized encoding
+encode = preprocessing.LabelEncoder()
+encode.fit(nyc_df.neighbourhood)
+nyc_df.neighbourhood=encode.transform(nyc_df.neighbourhood)
+
+encode = preprocessing.LabelEncoder()
+encode.fit(nyc_df.room_type)
+nyc_df.room_type=encode.transform(nyc_df.room_type)
+
+nyc_df.sort_values(by='price',ascending=True,inplace=True)
+
+# print(nyc_df.head(20))
+
+#Train linier regression model
+
+l_reg = LinearRegression()
+
+X = nyc_df[['host_id','neighbourhood_group','neighbourhood','latitude','longitude','room_type','minimum_nights','number_of_reviews','reviews_per_month','calculated_host_listings_count','availability_365']]
+y = nyc_df['price']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=101)
+
+l_reg.fit(X_train,y_train)
+
+#prediction
+
+predicts = l_reg.predict(X_test)
+
+print("Mean Squared Error: ", np.sqrt(metrics.mean_squared_error(y_test, predicts)))
+print("R2 Score: ", r2_score(y_test,predicts) * 100)
+print("Mean Absolute Error: ", mean_absolute_error(y_test,predicts))
+print("Mean Squareroot Error: ", mean_squared_error(y_test,predicts))
+
+#Actual Vs Predicted for Linear Regression
+lr_pred_df = pd.DataFrame({
+        'actual_values': np.array(y_test).flatten(),
+        'predicted_values': predicts.flatten()}).head(20)
+
+print(lr_pred_df.head(5))
+
+x = lr_pred_df.index
+width = 0.35
+fig, ax = plt.subplots()
+rects1 = ax.bar(x - width/2, lr_pred_df.actual_values, width, label='Actual Values')
+rects2 = ax.bar(x + width/2, lr_pred_df.predicted_values, width, label='Predicted Values')
+ax.set_ylabel('Price')
+ax.set_title('Actual Vs Predicted for Linear Regression')
+ax.set_xticks(x)
+ax.legend()
+fig.tight_layout()
+plt.show()
