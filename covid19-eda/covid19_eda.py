c9077992f7696e1da323e75b1e4cfb454bf5b585
dwiknrd <dwiknrd@gmail.com>
2020-10-07 16:58:01 +0700

    called api function

--- covid19-eda/covid19_eda.py
+++ covid19-eda/covid19_eda.py
@@ -12,3 +12,10 @@ def get_json(api_url):
 		return json.loads(response.content.decode('utf-8'))
 	else:
 		return None
+
+#called api function
+record_date = '2020-08-17'
+covid_url = 'https://covid19-api.org/api/status?date='+record_date
+df_covid_worldwide = pd.io.json.json_normalize(get_json(covid_url))
+print(df_covid_worldwide.head()
+)
