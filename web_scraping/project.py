464d4f6aa9ca424446d1ac858148881fd0f14e8f
dwiknrd <dwiknrd@gmail.com>
2020-10-26 10:36:12 +0700

    mapping web scraping step

--- web_scraping/project.py
+++ web_scraping/project.py
@@ -1,6 +1,23 @@
-#import library yang dibutuhkan
+#import library
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-print("dwik")
+#make request to the website
+website_url = requests.get('https://en.wikipedia.org/wiki/List_of_Nobel_laureates_in_Physics').text
+soup = BeautifulSoup(website_url, 'lxml')
+
+#take table with class named 'wikitable sortable'
+my_table = soup.find('table', {'class':'wikitable sortable'})
+
+#searh data
+
+
+#make an empty list
+
+
+# enter the data to the list
+
+
+
+#make a dataframe and extrat to csv file
